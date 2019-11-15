77d1fa849d7fc29b28873f8a099bccf40e004f23
tamanegi07 <tamanegi322@gmail.com>
2019-11-15 13:09:52 +0900

    automatic

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,9 +1,8 @@
-$(function() {
-
+$(function(){
   function buildMessage(message){
     let content = message.content? `<div class = 'message__lower-info__text'>${message.content}</div>` : "";
     let imagefile = message.image.url? `<img src="${message.image.url}", class = 'lower-message__image'>` : "";
-    let html =    `<div class="message">
+    let html =    `<div class="message" data-id="${message.id}">
                     <div class="message__upper-info">
                       <div class="message__upper-info__talker">
                       ${message.name}
@@ -18,32 +17,53 @@ $(function() {
                   </div>
                   </div>`
       return html;
-  }
-
-$("#new_message").on("submit", function(e){
-  e.preventDefault();
-  let formData = new FormData(this);
-  let url =  $(this).attr("action");
-  $.ajax({
-    type: 'POST',
-    url: url,
-    data: formData,
-    dataType: 'json',
-    processData: false,
-    contentType: false
-  })
-  .done(function(message){
-    let html = buildMessage(message);
-    $(".messages").append(html);
-    $("form")[0].reset();
-    $(".messages").animate({
-      scrollTop: $(".messages")[0].scrollHeight})
+  };
+  $("#new_message").on("submit", function(e){
+    e.preventDefault();
+    let formData = new FormData(this);
+    let url =  $(this).attr("action");
+    $.ajax({
+      type: 'POST',
+      url: url, 
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
     })
-  .always(function() {
-    $(".form__input--btn").removeAttr("disabled")
-  })
-  .fail(function(message){
-    alert("メッセージ送信に失敗しました")
-  })
-})
-}) 
+    .done(function(message){
+      let html = buildMessage(message);
+      $(".messages").append(html);
+      $("form")[0].reset();
+      $(".messages").animate({
+        scrollTop: $(".messages")[0].scrollHeight
+      });
+      })
+    .always(function() {
+      $(".form__input--btn").removeAttr("disabled")
+    })
+    .fail(function(){
+      alert("メッセージ送信に失敗しました")
+    })
+  });
+  let reloadMessages = function() {
+    let last_message_id = $(".message:last").data("id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages){
+      let insertHTML = "";
+      messages.forEach(function(message){
+        insertHTML = buildMessage(message)
+        $(".messages").append(insertHTML);
+      })
+        $(".messages").animate({scrollTop: $(".messages")[0].scrollHeight});
+      })
+    .fail(function(){
+      alert("メッセージ送信に失敗しました")
+    });
+  };
+  setInterval(reloadMessages, 5000);
+});
