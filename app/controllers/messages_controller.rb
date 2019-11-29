c270b861953fc1054a312d4e1bf19343a06a6d9e
tamanegi07 <tamanegi322@gmail.com>
2019-11-29 13:16:21 +0900

    直し

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,6 +5,7 @@ class MessagesController < ApplicationController
     @messages = @group.messages.includes(:user)    
   end
   def create
+    
     @message = @group.messages.new(message_params)
     if @message.save 
       respond_to do |format|
